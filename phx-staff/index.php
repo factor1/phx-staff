8978ea3ffbb6c74194713de941de9ff74aa1d262
Alexander Persky <alexander@factor1studios.com>
2015-12-28 13:20:55 -0700

    Dev Notes complete
    - revisted the pages with the complete dev notes and made tweaks

--- phx-staff/index.php
+++ phx-staff/index.php
@@ -18,27 +18,37 @@ get_header();
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <article id="post-<?php the_ID(); ?>" <?php post_class() ?>>
             <?php if(has_post_thumbnail()) : ?><!-- With Featured Image -->
-              <div class="row">
-                <div class="medium-3 medium-offset-1 columns">
-                  <?php the_post_thumbnail(); ?>
-                </div>
-                <div class="medium-7 end columns">
-                  <h1 class="blogroll-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                  <?php get_template_part('parts/meta' ); ?>
-                  <?php the_excerpt(); ?>
-                  <div class="read-more">
-                    <a href="<?php the_permalink(); ?>" class="button round">Read More <i class="fa fa-arrow-right"></i></a>
+              <div class="row with-thumbnail">
+                <div class="medium-10 medium-centered columns">
+                  <div class="row">
+                    <div class="medium-4 columns">
+                      <?php the_post_thumbnail(); ?>
+                    </div>
+                    <div class="medium-8 columns">
+                      <h2 class="blogroll-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                      <?php get_template_part('parts/meta' ); ?>
+                      <?php the_excerpt(); ?>
+                      <div class="read-more">
+                        <a href="<?php the_permalink(); ?>" class="button round">Read More <i class="fa fa-arrow-right"></i></a>
+                      </div>
+                      <div class="tags">
+                        <?php the_tags( '', ', ', '' ); ?>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             <?php else : ?><!-- no Featured Image -->
               <div class="row">
                 <div class="medium-10 medium-centered columns">
-                  <h1 class="blogroll-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                  <h2 class="blogroll-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                   <?php get_template_part('parts/meta' ); ?>
                   <?php the_excerpt(); ?>
                   <div class="read-more">
                     <a href="<?php the_permalink(); ?>" class="button round">Read More <i class="fa fa-arrow-right"></i></a>
+                  </div>
+                  <div class="tags">
+                    <?php the_tags( '', ', ', '' ); ?>
                   </div>
                 </div>
               </div>
