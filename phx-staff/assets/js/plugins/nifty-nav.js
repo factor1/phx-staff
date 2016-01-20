ae35c308fff46c6f3f0bf842dad0da29860a7f8f
Eric Stout <ericwstout@gmail.com>
2016-01-20 11:43:01 -0700

    Updates nifty nav version

--- phx-staff/assets/js/plugins/nifty-nav.js
+++ phx-staff/assets/js/plugins/nifty-nav.js
@@ -1,58 +1,92 @@
-////////////////////////////////////////////////////////////////////////
-///////////------Factor1 Nav------//////////////////////////////////////
-// a jquery tool by Eric Stout (@buschschwick) and Matt Adams (@mattada)
-// version 1.4 ////////////////////////////////////////////////////////
-// http://factor1studios.com  ////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-jQuery( document ).ready( function( $ ) {
-  $( document ).ready( function() {
+// function we use to remove the mask
+var niftyRemove = function(){
+	$('.nifty-mask').remove();
+};
 
-    var remove = function() { //function we use to remove the mask
-      $( '.mask' ).remove();
-    };
+var niftyUnMaskIt = function(){
+	$('.nifty-mask').animate({opacity:0.0});
+	setTimeout(niftyRemove, 800);
+};
 
-    maskit = function() {
-      $( '.mask' ).animate( {
-        opacity: .0
-      } );
-      setTimeout( remove, 800 );
-    };
-    i = 0;
+// setting up our variables for submenu toggles
+var mobileNavItem = $('.nifty-panel ul li');
+var $mobileClicked;
 
-    $( '#nifty-nav-toggle' ).click( function() { //listen for the click on the menu icon
-      i++; // Add 1 to the count
-      $( '#nifty-nav-toggle' ).toggleClass( 'active' );
-      $( '.nifty-panel' ).slideToggle( 500 ).css( "position",
-        "relative" ); //toggle the nav up/down
+// when the document is ready lets go!
+$(document).ready(function(){
 
-      // Check if the counter is even or odd
-      var isEven = function( clickCounter ) {
-        return ( clickCounter % 2 === 0 ) ? true : false;
-      };
+	i = 0;
 
-      if ( isEven( i ) === false ) {
-        $( 'body' ).append( '<div class="mask"></div>' ); //lets create a nice mask to dim the content
-        $( '.mask' ).animate( {
-          opacity: 1.0
-        } );
-      } else {
-        maskit();
-      }
+	// listen for the click on the menu icon
+	$('#nifty-nav-toggle').click(function(){
 
-      //or alternatively you can click the mask and make that shit go away.
-      $( '.mask' ).click( function() { //listen for the click on the mask
-        $( '.nifty-panel' ).slideUp( 500 ); //pull up the nav
-        maskit();
-        $( '#nifty-nav-toggle' ).removeClass( 'active' ); //mmm hamburger
-        i++; // Add 1 to the count
-      } );
+		// Add 1 to the count
+		i++;
 
-      $( '.nav-item' ).click( function() { //oh snap, you want to close the nav on a link click?
-        $( '.nifty-panel' ).slideUp( 500 ); //pull up the nav
-        maskit();
-        $( '#nifty-nav-toggle' ).removeClass( 'active' );
-        i = 0; // reset the counter
-      } );
-    } ); //close up everything
-  } );
-} );
+		$('#nifty-nav-toggle').toggleClass('nifty-active');
+
+		// toggle the nav up/down
+		$('.nifty-panel').slideToggle(500).css("position", "absolute");
+
+		// Check if the counter is even or odd
+		var isEven = function(clickCounter){
+			return (clickCounter%2 === 0) ? true : false;
+		};
+
+		if (isEven(i) === false){
+
+			//lets create a nice mask to dim the content
+			$('body').append('<div class="nifty-mask"></div>');
+			$('.nifty-mask').animate({opacity:1.0});
+		} else {
+			niftyUnMaskIt();
+		}
+
+		// or alternatively you can click the mask and make that shit go away.
+		$('.nifty-mask').click(function(){
+
+			//slide up the nav panel
+			$('.nifty-panel').slideUp(500);
+
+			//remove the mask
+			niftyUnMaskIt();
+
+			// remove the active class on the hamburger mmmm hamburger
+			$('#nifty-nav-toggle').removeClass('nifty-active');
+
+			i++;  // Add 1 to the count
+		});
+
+	});
+	// close up our listener for nav toggle
+
+
+
+	/*
+	Oh snap! You want to close the menu when clicking a nav-item?
+	We got you... this is usefull when your site is a single page layout
+	and you want to jump down to an anchor tag instead of a different page.
+	*/
+	$('.nifty-nav-item').click(function(){
+
+		//slide up our panel
+		$('.nifty-panel').slideUp(500);
+		niftyUnMaskIt();
+		$('#nifty-nav-toggle').removeClass('nifty-active');
+		i = 0;  // reset the counter
+	});
+
+
+	/*
+	Listen for any menu items that have children and allow them
+	to toggle open when clicked. This is by default setup for
+	Wordpress menus, however if you're not using Wordpress just
+	make sure your classes and structure matches.
+	*/
+	mobileNavItem.on('click', function(){
+		$mobileClicked = $(this);
+		$mobileClicked.find('.sub-menu').slideToggle();
+		$mobileClicked.find('a').toggleClass('menu-opened');
+	});
+
+});
--- phx-staff/assets/js/theme.js
+++ phx-staff/assets/js/theme.js
@@ -13,12 +13,12 @@ jQuery(document).ready(function($) {
   $('.career-search').matchHeight();
 
   // expand the mobile nav
-  $( '.nifty-panel .menu-item-has-children > a' ).on( 'click',
-    function( e ) {
-      e.preventDefault();
-      // $( this ).parent().children( '.fa' ).toggleClass( 'active' );
-      $( this ).parent().children( '.sub-menu' ).toggleClass( 'expand' );
-    } );
+  // $( '.nifty-panel .menu-item-has-children > a' ).on( 'click',
+  //   function( e ) {
+  //     e.preventDefault();
+  //     // $( this ).parent().children( '.fa' ).toggleClass( 'active' );
+  //     $( this ).parent().children( '.sub-menu' ).toggleClass( 'expand' );
+  //   } );
 
 
 });
