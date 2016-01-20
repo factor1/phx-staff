ddf0dbdab8953474fc723c9a9a60969d63cefd0e
Eric Stout <ericwstout@gmail.com>
2016-01-20 11:50:43 -0700

    Removes old JS

--- phx-staff/assets/js/theme.js
+++ phx-staff/assets/js/theme.js
@@ -11,14 +11,4 @@ jQuery(document).ready(function($) {
   $('.job-item').matchHeight();
   $('.value-item').matchHeight();
   $('.career-search').matchHeight();
-
-  // expand the mobile nav
-  // $( '.nifty-panel .menu-item-has-children > a' ).on( 'click',
-  //   function( e ) {
-  //     e.preventDefault();
-  //     // $( this ).parent().children( '.fa' ).toggleClass( 'active' );
-  //     $( this ).parent().children( '.sub-menu' ).toggleClass( 'expand' );
-  //   } );
-
-
 });
