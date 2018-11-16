7f48c88529c60960f94484f4d08ea865c4ea750f
Nick Exposito <nexpos@gmail.com>
2018-11-16 11:13:27 -0700

    updated landing page template to include site header and footer

--- phx-staff/page-templates/landing.php
+++ phx-staff/page-templates/landing.php
@@ -8,24 +8,23 @@ $button_link = get_field('button_link')
 ?>
 
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-</head>
-<body>
-<?php while ( have_posts() ) : the_post(); ?>
-    <section class="landing">
-      <div class="row">
-        <div class="small-6 small-centered columns">
-          <div class="landing-content">
-            <?php the_content(); ?>
-            <a href="<?php echo $button_link; ?>" class="button"><?php echo $button_text; ?></a>
+  <head>
+      <meta charset="<?php bloginfo( 'charset' ); ?>">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <?php wp_head(); ?>
+  </head>
+  <?php get_header(); ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+      <section class="landing" style="background-color: #000;">
+        <div class="row">
+          <div class="small-6 small-centered columns">
+            <div class="landing-content">
+              <?php the_content(); ?>
+              <!-- <a href="<?php //echo $button_link; ?>" class="button"><?php //echo $button_text; ?></a> -->
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-<?php endwhile; ?>
-<?php wp_footer(); ?>
-</body>
+      </section>
+  <?php endwhile; ?>
+  <?php get_footer(); ?>
 </html>
